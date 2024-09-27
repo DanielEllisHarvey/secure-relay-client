4e529028af717bba91de3e3130d0dea94dfd4c0a
danielellisharvey <danielellisharvey@outlook.com>
2024-09-27 15:09:18 +0100

    Integrated ws sending, recieving, and decryption

--- ws-server.py
+++ ws-server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket, Response
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import base64
 from cryptography.hazmat.primitives import serialization, hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import ec
@@ -16,23 +16,38 @@ class ConnectionManager:
         self.named_connections: dict = {}
 
     async def connect(self, websocket: WebSocket):
-        await websocket.accept()
         self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
+        self.unalias(websocket)
         #  self.named_connections
 
-    async def alias(self, name: str, websocket: WebSocket):
+    def unalias(self, websocket: WebSocket):
+        key = self.named_connections[websocket]
+        self.named_connections.pop(websocket)
+        self.named_connections.pop(key)
+
+    def alias(self, name: str, websocket: WebSocket):
         self.named_connections.update({name: websocket})
         self.named_connections.update({websocket: name})
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
+        
+    async def send_message_by_key_json(self, message: str, key: str, return_address: WebSocket):
+        if key in self.named_connections:
+            websocket = self.named_connections[key]
+            await websocket.send_json(message)
+        await return_address.send_json({"type": "recipientNotFound"})
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
+    
+    async def broadcast_json(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_json(message)
 
 manager = ConnectionManager()
 
@@ -43,30 +58,32 @@ def return_pubkey():
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket, key: str):
-    await manager.connect(websocket)
+    await websocket.accept()
     # print(manager.active_connections)
     # await manager.alias(await websocket.receive_text(), websocket)
-    print(key)
-    bytes_key = base64.b64decode(key.replace(" ", "+"))
-    print(bytes_key)
+    parsed_key = key.replace(" ", "+")
+    bytes_key = base64.b64decode(parsed_key)
     peer_public_key = serialization.load_der_public_key(bytes_key)
     sharedKey = privateKey.exchange(ec.ECDH(), peer_public_key)[0:32]
-    randomBytesChallenge = os.urandom(64)
-    await websocket.send_json({"p384key": str(publicKey, "utf-8"), "randomBytesChallenge": str(base64.b64encode(randomBytesChallenge), "utf-8")})
+    randomBytesChallenge = os.urandom(1024)
+    await websocket.send_json({"type": "challenge", "p384key": str(publicKey, "utf-8"), "randomBytesChallenge": str(base64.b64encode(randomBytesChallenge), "utf-8")})
     challengeResponse = await websocket.receive_bytes()
     hashInstance = hmac.HMAC(sharedKey, hashes.SHA512())
     hashInstance.update(randomBytesChallenge)
     digest = hashInstance.finalize()
     if digest == challengeResponse:
-        await manager.alias(websocket, key)
-    else: websocket.close(reason="Wrong challenge code.")
-    while True:
-        message = await websocket.receive_json()
-        await manager.send_personal_message(base64.b64encode(sharedKey), websocket)
+        manager.alias(websocket,parsed_key)
+        await manager.connect(websocket)
+        print("challenge complete")
+        print(manager.named_connections)
+    # else: websocket.close(reason="Wrong challenge code.")
+    # while text != "ready": text = await websocket.receive_text()
+    try:
+        while True:
+            message = await websocket.receive_json()
+            print(message)
+            await manager.send_message_by_key_json(message, message["to"], websocket)
+            # await manager.send_personal_message(base64.b64encode(sharedKey), websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
         
-            # name = manager.named_connections[websocket]
-            # if "body" not in data: await manager.send_personal_message("Error: No body in message body")
-            # if (("recipient" in data) and (data["recipient"] in manager.named_connections)):
-            #     await manager.send_personal_message(f"{manager.named_connections[websocket]}: {data["body"]}", manager.named_connections[data["recipient"]])
-            # else: await manager.send_personal_message("Error: Undefined or invalid recipient in message body", websocket)
-        # print(data)
